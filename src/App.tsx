782c55a62fe3ffae8fa3ef88f81b03c58c81bda8
dantelacu2 <55060638+dantelacu2@users.noreply.github.com>
2020-10-24 10:28:06 -0700

    added login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, makeStyles, TextField } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    height: '100%',
+    display: 'grid',
+  },
+  inputWrapper: {
+    margin: 'auto',
+    textAlign: 'center',
+  }
+});
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div className={classes.inputWrapper}>
+        <h2>Enter your gmail login & API key</h2>
+        <form>
+          <TextField required label="Email Address" variant="outlined" id="email"/>
+            <br/><br />
+          <TextField required label="Password" variant="outlined" id="password"/>
+            <br/><br />
+          <TextField required label="API Key" variant="outlined" id="apiKey"/>
+            <br/><br />
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
